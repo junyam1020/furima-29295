3d509db70fc1cd841d66e20beb204fe1408b716e
junyam1020 <junyam7489@gmail.com>
2020-08-31 23:43:05 +0900

    出品ページの選択肢追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipper_area
+  belongs_to_active_hash :shipment_date
+
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
@@ -9,6 +16,13 @@ class Item < ApplicationRecord
   validates :shipment_date, presence: true
   validates :price, presence: true
   validates :user, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 0 }
+  validates :status_id, numericality: { other_than: 0 }
+  validates :delivery_fee_id, numericality: { other_than: 0 }
+  validates :shipper_area_id, numericality: { other_than: 0 }
+  validates :shipment_date_id, numericality: { other_than: 0 }
 
   belongs_to :user
   has_one :deal
