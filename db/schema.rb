74668ffef4a40aa5a61bab9ec96392bddef02539
junyam1020 <junyam7489@gmail.com>
2020-09-07 17:04:43 +0900

    Revert "Revert "画像登録機能漏れの反映""
    This reverts commit 1fdf88809c3b4f161fcdb9fda22a3b08088e39af.

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_09_07_065437) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
