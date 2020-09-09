119845a904f64df350b160065638334e445bad26
junyam1020 <junyam7489@gmail.com>
2020-09-09 15:25:38 +0900

    修正事項の差し戻し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
